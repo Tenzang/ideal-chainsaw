d8ddf8d14c1466edc713f7df42bcd8282085108e
Loden <loden.gendun@students.mq.edu.au>
2022-03-22 09:03:42 +1100

    Initial Styling

--- scripts/DOM-handler.js
+++ scripts/DOM-handler.js
@@ -1,40 +1,45 @@
-console.log('DOM-handler connected');
-let turn = true;
+let xTurn = true; // Boolean to track player turns.
 
-$('.tile').on('click', function() {
-    const $tile = $(this);
+$(document).ready(function() {
 
-    // Check class for coordinates of clicked node.
-    const x = $tile[0].classList[1][1];
-    const y = $tile[0].classList[2][1];
-    console.log("coordinates:", x, ",", y);
+    $('.tile').on('click', function() {
+        const $tile = $(this);
+    
+        // Check class for coordinates of clicked node.
+        const x = $tile[0].classList[1][1];
+        const y = $tile[0].classList[2][1];
+    
+        // Run move function with coordinates the insert piece into node
+        if (!$tile.text()) {
+    
+            if (xTurn) {
+                const outcome = game.xMove(x, y);
 
-    // Run move function with coordinates the insert piece into node
-    if (!$tile.text()) {
+                $(this).html($('<p class="piece">X</p>'));
+                $('.results').html(`<h3>${outcome.message}</h3>`);
+    
+                // Game Over, stop listener
+                if (outcome.gameOver) {
+                    $('.tile').off();
+                };
+    
+            } else {
+                const outcome = game.oMove(x, y);
+                
+                $(this).html($('<p class="piece">O</p>'));
+                $('.results').html(`<h3>${outcome.message}</h3>`);
 
-        if (turn) {
-            const outcome = game.xMove(x, y);
-            $(this).html($('<p class="piece">X</p>'));
-            $('.results').text(outcome.message);
-            console.log(outcome);
-
-            // Game Over, stop listener
-            if (outcome.gameOver) {
-                $('.tile').off();
+                // Game Over, stop listener
+                if (outcome.gameOver) {
+                    $('.tile').off();
+                };
+    
             };
 
-        } else {
-            const outcome = game.oMove(x, y);
-            $(this).html($('<p class="piece">O</p>'));
-            $('.results').text(outcome.message);
-            console.log(outcome);
-
-            // Game Over, stop listener
-            if (outcome.gameOver) {
-                $('.tile').off();
-            };
-
+            xTurn = !xTurn;
+            
         };
-        turn = !turn;
-    };
+
+    });
+
 });
--- scripts/business-logic.js
+++ scripts/business-logic.js
@@ -1,10 +1,24 @@
-console.log('business-logic connected');
-
 const boardSetup = new Array(3);
 boardSetup[0] = new Array(3);
 boardSetup[1] = new Array(3);
 boardSetup[2] = new Array(3);
 
+// Checks if the values in a line are identical.
+const winnerFound = function(line) {
+    let output = true;
+
+    for (let i = 0; i < line.length; i++) {
+        
+        if (!line[i] || line[i] != line[0]) {
+            output = false;
+        };
+
+    };
+
+    return output;
+};
+
+// Checks if there is a winning line present on the board.
 const winnerCheck = function() {
     
     for (let i = 0; i < game.board.length; i++) {
@@ -19,58 +33,21 @@ const winnerCheck = function() {
             colToCheck.push(game.board[i][j]);
             rowToCheck.push(game.board[j][i]);
             descDiagCheck.push(game.board[j][j]);
-            ascDiagCheck.push(game.board[j][game.board.length- 1 - j])
+            ascDiagCheck.push(game.board[j][game.board.length - 1 - j])
 
         };
         
-        // Checks if a value is equal to first element in the row/col being checked.
-        const winnerFound = function(line) {
-            let output = true;
-
-            for (let i = 0; i < line.length; i++) {
-                
-                if (!line[i] || line[i] != line[0]) {
-                    output = false;
-                };
-
-            };
-
-            return output;
-        };
-
         // returns true if all values in a row, column or diagonal are the same.
-        if ( winnerFound(colToCheck) ) {
-            console.log('col victory');
-            console.log(colToCheck);
-            return true;
-        };
-
-        if ( winnerFound(rowToCheck) ) {
-            console.log('row victory');
-            console.log(rowToCheck);
-            return true;
-        };
-
-        if ( winnerFound(descDiagCheck) ) {
-            console.log('desc victory');
-            console.log(descDiagCheck);
-
-            return true;
-        };
-
-        if ( winnerFound(ascDiagCheck) ) {
-            console.log('asc victory');
-            console.log(ascDiagCheck);
+        if ( winnerFound(colToCheck) || winnerFound(rowToCheck) || winnerFound(descDiagCheck) || winnerFound(ascDiagCheck) ) {
             return true;
         };
 
     };
 
-
     return false;
-
 };
 
+// Checks if the board tiles are all occupied.
 const drawCheck = function() {
 
     for (let i = 0; i < game.board.length; i++) {
@@ -88,6 +65,7 @@ const drawCheck = function() {
     return true;
 };
 
+// Generates a report object for delivery to DOM-handler.js
 const generateOutcome = function(message) {
     const output = {};
     
@@ -107,14 +85,14 @@ const generateOutcome = function(message) {
     return output;
 };
 
+// Main game object.
 const game = {
     
-    board: boardSetup,
+    board: boardSetup, // Stores the board state.
 
-    gameOver: false,
+    gameOver: false, // Stores whether game has ended.
 
-    // Place piece at 0-index'd position on the board.
-    // Returns message object if terminating move played, else false.
+    // Move methods place piece at 0-index'd position on the board and returns the outcome of the move in an object.
     xMove: function(x, y) {
         
         this.board[y][x] = 'X';
